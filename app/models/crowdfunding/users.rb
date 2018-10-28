cae61e8df1d3ccb0ce9ac9e52e6255fbf78df836
intuitive-programmer <steven@intuitiveprogrammer.com>
2018-10-28 12:52:38 +0000

    Completed user.rb

--- app/models/crowdfunding/users.rb
+++ app/models/crowdfunding/users.rb
@@ -16,6 +16,29 @@ class User
         Pledge.new(self, project, amount)
     end
 
+    def self.highest_pledge
+        Pledge.all.max_by { |p| p.amount }.user_id
+    end
+
+    def self.pledge_count
+        pledge_count = {}
+        Pledge.all
+            .map { |p| p.user_id.name }
+            .each do |u|
+                pledge_count[u] ||= 0
+                pledge_count[u] += 1
+            end
+        pledge_count
+    end
+
+    def self.multi_pledger
+        User.pledge_count.keep_if { |u, c| c > 1 }
+    end
+
+    def self.project_creator
+        Project.all.map { |p| p.creator }
+    end
+
     def self.all
         @@all
     end
